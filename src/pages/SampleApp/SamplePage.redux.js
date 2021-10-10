4c27c99992d99ca487eeac2c783dc34cb5e4a29f
Sagar Jhobalia <sagar.jhobalia@gmail.com>
2021-10-10 18:32:39 +0530

    Sets up an example of synthetic-redux

---
+++ src/pages/SampleApp/SamplePage.redux.js
@@ -0,0 +1,25 @@
+import { combineReducers } from 'redux';
+import { all } from 'redux-saga/effects';
+import { SAMPLE } from 'redux/Types';
+import { SyntheticRedux } from 'synthetic-redux';
+import sample from 'utils/urlConstants';
+
+
+const fetchSampleStatus = new SyntheticRedux({
+  type: SAMPLE.FETCH_STATUS,
+  url: sample.status
+}, 'response');
+
+export {
+  fetchSampleStatus
+};
+
+export const SampleReducer = combineReducers({
+  status: fetchSampleStatus.reducer
+});
+
+export function* SampleSaga() {
+  yield all([
+    fetchSampleStatus.sagaEffect
+  ]);
+}
