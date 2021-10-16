ef2721b089be70f148adc027e3f3851ed463c874
Sagar Jhobalia <sagar.jhobalia@gmail.com>
2021-10-16 13:08:44 +0530

    Adds disclaimer

--- src/components/SampleApp/HelloWorld.js
+++ src/components/SampleApp/HelloWorld.js
@@ -1,6 +1,9 @@
+import Box from '@material-ui/core/Box';
+import Tooltip from '@material-ui/core/Tooltip';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import { HelpOutline } from '@material-ui/icons';
 import React from 'react';
 
 
@@ -8,8 +11,15 @@ const HelloWorld = ({ status }) => (
   <Card>
     <CardContent>
       <Typography paragraph variant='h1' color='primary'>Welcome To React-Gold</Typography>
-      <Typography variant='caption'>Gold Standard for a SPA in 2020</Typography>
-      <Typography variant='subtitle1'>{`API Status: ${status}`}</Typography>
+      <Box display='flex' justifyContent='flex-start'>
+        <Typography component='span' variant='subtitle1'>Gold Standard for a SPA in 2020</Typography>
+        <Tooltip title='This is a boilerplate and code structure demonstration.
+      The packages have not been updated since 2020'
+        >
+          <HelpOutline style={{ fontSize: 15 }} />
+        </Tooltip>
+      </Box>
+      <Typography variant='body2'>{`API Status: ${status}`}</Typography>
     </CardContent>
   </Card>
 );
